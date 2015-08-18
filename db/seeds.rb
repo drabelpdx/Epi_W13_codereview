c2fce300e11e10e817e4328ab512421cc5f980e0
DavidAbel <drabelpdx@gmail.com>
2015-08-17 21:01:09 -0700

    Adds Ember101 to projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,13 @@ categories = Category.create([
 {name: "Ember", description: "Ember was my first framework. "}])
 
 projects = Project.create([
-{name: "Portfolio", description:"This multi-week project you are looking at right now.", link:"https://github.com/drabelpdx/Epi_W13_codereview.git", url: "https://limitless-bayou-3984.herokuapp.com", image: "http://www.abelpdx.com/images/portfolio.png", category_id:1 },
 {name: "My Record Crate", description:"This is a rails app that uses the API from Discogs to display artists and album information.", link:"https://github.com/drabelpdx/MyRecordCrate.git", url: "https://agile-retreat-9554.herokuapp.com", image: "http://www.abelpdx.com/images/recordcrate.png", category_id:1 },
+{name: "Portfolio", description:"This project you are looking at right now. It provides several work samples with links to the github code and live sites where possible.", link:"https://github.com/drabelpdx/Epi_W13_codereview.git", url: "", image: "http://www.abelpdx.com/images/portfolio.png", category_id:1 },
 {name: "Pisa Pizza", description: "This app allows the user to order pizzas online. The user can order mulitple pizzas with a variety of toppings. The app will display the user's order and a total cost on completion.", link:"https://github.com/drabelpdx/Epi_W7_codereview", url: "http://www.abelpdx.com/pizza.html", image: "http://www.abelpdx.com/images/pizza.png", category_id:2},
 {name: "Tic-Tac-Toe", description: "A simple game of Tic-Tac-Toe in JavaScript.", link:"https://github.com/drabelpdx/Epi_W7_tictactoe", url: "http://www.abelpdx.com/tictactoe.html", image: "http://www.abelpdx.com/images/tictactoe.png", category_id:2},
 {name: "Find and Replace", description: "This app allows the user to submit a sentence, a word to search for and a replacement word. The app will find all occurances of that word and replace with the given replacement word. Then it will return the new sentence back to the user.", link:"https://github.com/drabelpdx/Epi_W6_codereview", url: "http://www.abelpdx.com/findreplace.html", image: "http://www.abelpdx.com/images/FindReplace.png", category_id:2}])
+{name: "Ember101", description: "This is a build of the Ember101 excercise by Adolfo Builes and deployed to Heroku.", link:"https://github.com/drabelpdx/Epi_W15_Ember101", url: "https://secret-escarpment-1137.herokuapp.com", image: "http://www.abelpdx.com/images/ember101.png", category_id:3}])
+
 
 posts = Post.create([
 {title: "My first post", body: "So what do you think so far?"},
